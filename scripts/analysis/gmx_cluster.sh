554f893f2ceda4dd1ef1ea6cd01a618e2c2bc247
FridrichMethod <zhaoyangli@stanford.edu>
2026-02-19 20:09:20 -0800

    Update gmx_cluster.sh to modify output file names for clustering analysis
    - Changed output filenames for distance and size metrics to improve clarity and consistency.
    - Added additional gracebat commands to generate PNG visualizations for the updated output files.

--- scripts/analysis/gmx_cluster.sh
+++ scripts/analysis/gmx_cluster.sh
@@ -10,8 +10,8 @@ printf "Backbone\nSystem\n" | gmx cluster \
     -f "${PRODUCTION}_complex.xtc" \
     -cl "${PRODUCTION}_cluster.pdb" \
     -clid "${PRODUCTION}_clid.xvg" \
-    -dist "${PRODUCTION}_cldist.xvg" \
-    -sz "${PRODUCTION}_clsz.xvg" \
+    -dist "${PRODUCTION}_dist.xvg" \
+    -sz "${PRODUCTION}_size.xvg" \
     -method gromos \
     -b 0 \
     -cutoff 0.2
@@ -20,3 +20,13 @@ gracebat \
     -nxy "${PRODUCTION}_clid.xvg" \
     -hdevice PNG \
     -printfile "${PRODUCTION}_clid.png"
+
+gracebat \
+    -nxy "${PRODUCTION}_dist.xvg" \
+    -hdevice PNG \
+    -printfile "${PRODUCTION}_dist.png"
+
+gracebat \
+    -nxy "${PRODUCTION}_size.xvg" \
+    -hdevice PNG \
+    -printfile "${PRODUCTION}_size.png"
