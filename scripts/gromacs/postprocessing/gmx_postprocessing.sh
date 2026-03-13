6c18e6ec8bbd43bf11562901f465afee7ceadbd2
FridrichMethod <zhaoyangli@stanford.edu>
2026-03-13 11:22:04 -0700

    Fix syntax in gmx_postprocessing.sh for consistent TPR file reference
    - Updated the script to ensure the TPR file reference is consistently formatted as "${PRODUCTION}.tpr" instead of "${PRODUCTION}".tpr.
    - This change improves script reliability and prevents potential errors during execution of GROMACS commands.

--- scripts/gromacs/postprocessing/gmx_postprocessing.sh
+++ scripts/gromacs/postprocessing/gmx_postprocessing.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 PRODUCTION=step5_production
 
 # gmx select \
-#     -s "${PRODUCTION}".tpr \
+#     -s "${PRODUCTION}.tpr" \
 #     -on index.ndx \
 #     -select '
 #         "SOLU" (group 1 or group 13 or group 14)  # choose the group you want to extract
@@ -14,7 +14,7 @@ PRODUCTION=step5_production
 # Select the protein chain A and its backbone
 # CHARMM-GUI predefines the protein chain A as molecule 1
 gmx select \
-    -s "${PRODUCTION}".tpr \
+    -s "${PRODUCTION}.tpr" \
     -on index_protein_chain_A.ndx \
     -select '
         "System" group "System";
@@ -27,7 +27,7 @@ gmx select \
 # This prevents the protein assembly from being artificially split by periodic boundary conditions
 # See https://gromacs.bioexcel.eu/t/protein-come-out-of-the-box-but-trjconv-command-didnt-fix-the-problem/3851/8
 printf "Chain_A\nSystem\n" | gmx trjconv \
-    -s "${PRODUCTION}".tpr \
+    -s "${PRODUCTION}.tpr" \
     -f "${PRODUCTION}".xtc \
     -o "${PRODUCTION}_center.xtc" \
     -n index_protein_chain_A.ndx \
@@ -37,7 +37,7 @@ printf "Chain_A\nSystem\n" | gmx trjconv \
 
 # Fit the trajectory
 printf "Chain_A_BB\nSystem\n" | gmx trjconv \
-    -s "${PRODUCTION}".tpr \
+    -s "${PRODUCTION}.tpr" \
     -f "${PRODUCTION}_center.xtc" \
     -o "${PRODUCTION}_fit.xtc" \
     -n index_protein_chain_A.ndx \
@@ -45,14 +45,14 @@ printf "Chain_A_BB\nSystem\n" | gmx trjconv \
 
 # Extract the complex (solute)
 printf "SOLU\n" | gmx trjconv \
-    -s "${PRODUCTION}".tpr \
+    -s "${PRODUCTION}.tpr" \
     -f "${PRODUCTION}_fit.xtc" \
     -o "${PRODUCTION}_complex_fit.xtc" \
     -n index.ndx
 
 # Extract the first frame
 printf "SOLU\n" | gmx trjconv \
-    -s "${PRODUCTION}".tpr \
+    -s "${PRODUCTION}.tpr" \
     -f "${PRODUCTION}_fit.xtc" \
     -o "${PRODUCTION}_complex_fit.pdb" \
     -n index.ndx \
@@ -60,7 +60,7 @@ printf "SOLU\n" | gmx trjconv \
 
 # Create a solute-only TPR for use with the extracted complex trajectory
 printf "SOLU\n" | gmx convert-tpr \
-    -s "${PRODUCTION}".tpr \
+    -s "${PRODUCTION}.tpr" \
     -o "${PRODUCTION}_complex_fit.tpr" \
     -n index.ndx
 
